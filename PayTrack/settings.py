d760ba23b45118a1c59ac3beb212c99f9757ebe2
Emmanuel Oluyemi <e8060097@gmail.com>
2026-05-31 12:28:02 +0100

    Add Whitenoise for static file storage

--- PayTrack/settings.py
+++ PayTrack/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
