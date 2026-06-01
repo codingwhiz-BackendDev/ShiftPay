ea12e62b8c392cbf108676415a6ad72edce2712a
Emmanuel Oluyemi <e8060097@gmail.com>
2026-06-01 12:03:49 +0100

    Remove auto-migration on WSGI startup
    Removed auto-migration logic from WSGI file.

--- PayTrack/wsgi.py
+++ PayTrack/wsgi.py
@@ -3,12 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PayTrack.settings')
 
-# Auto-migrate on startup so Neon tables are created automatically
-try:
-    from django.core.management import call_command
-    call_command('migrate', '--run-syncdb', verbosity=0)
-    print("Migrations complete")
-except Exception as e:
-    print(f"Migration error: {e}")
-
 application = get_wsgi_application()
